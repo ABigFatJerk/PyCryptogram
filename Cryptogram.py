e8602deeb975af5094c37c1f22914b8b54c40437
ABigFatJerk <mccoder@outlook.com>
2016-03-12 23:57:51 -0500

    Got grid letters working, plus letter selection/highlighting

--- Cryptogram.py
+++ Cryptogram.py
@@ -1,28 +1,34 @@
 import Tkinter
+import string
 import CryptogramView
 import CryptogramModel
 
-# TODO gotta be a better way
-ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+ALPHABET = list(string.ascii_uppercase)
 
 
 class Cryptogram(object):
     def __init__(self, parent):
         self._parent = parent
+        self._letter_dict = {el: [] for el in ALPHABET}
+        self._selected_letter = None
         self._view = CryptogramView.CryptogramView(self, self._parent)
         self._model = CryptogramModel.CryptogramModel(self)
         self._setup_new_game()
 
     def _setup_new_game(self):
+        self._view.set_crypto_string("THE QUICK BROWN. FOX JUMPS, OVER THE LAZY! DOG")
         pass
 
     def register_letter(self, letter_view):
-        print "Letter view with letter " + letter_view.get_crypto_letter() + " registered itself"
-        pass
+        self._letter_dict[letter_view.get_crypto_letter()].append(letter_view)
 
     def handle_letter_click(self, letter_view):
-        print "Letter view with letter " + letter_view.get_crypto_letter() + " clicked"
-        pass
+        if self._selected_letter is not None:
+            for old_letter_view in self._letter_dict[self._selected_letter]:
+                old_letter_view.deselect()
+        self._selected_letter = letter_view.get_crypto_letter()
+        for new_letter_view in self._letter_dict[self._selected_letter]:
+            new_letter_view.select()
 
 if __name__ == '__main__':
     root = Tkinter.Tk()
--- CryptogramView.py
+++ CryptogramView.py
@@ -13,7 +13,8 @@ class CryptogramView(object):
         self._grid_frame = Tkinter.Frame(parent)
         self._grid_view = GridView(self._grid_frame, self._controller)
         self._key_frame.grid(row=0, column=0)
-        self._grid_frame.grid(row=1, column=0)
+        Tkinter.Label(parent, height=2).grid(row=1, column=0)
+        self._grid_frame.grid(row=2, column=0)
 
     def set_crypto_string(self, crypto_string):
         self._grid_view.set_crypto_string(crypto_string)
@@ -28,7 +29,7 @@ class KeyView(object):
             column = index % LETTER_WIDTH
             frame = Tkinter.Frame(self._parent_frame)
             letter_view = LetterView(frame, letter, self._controller)
-            controller.register_letter(letter_view)
+            self._controller.register_letter(letter_view)
             frame.grid(row=row, column=column)
 
 
@@ -38,19 +39,30 @@ class GridView(object):
         self._controller = controller
 
     def set_crypto_string(self, crypto_string):
-        pass # TODO
+        for (index, letter) in enumerate(crypto_string):  # TODO duplicated in KeyView init
+            row = index / LETTER_WIDTH
+            column = index % LETTER_WIDTH
+            frame = Tkinter.Frame(self._parent_frame)
+            LetterView(frame, letter, self._controller)
+            frame.grid(row=row, column=column)
 
 
 class LetterView(object):
     def __init__(self, parent_frame, crypto_letter, controller):
         self._parent_frame = parent_frame
         self._crypto_letter = crypto_letter
-        self._plaintext_letter = '?'
+        self._is_alpha = crypto_letter in Cryptogram.ALPHABET
+        if self._is_alpha:
+            self._plaintext_letter = '?'
+        else:
+            self._plaintext_letter = ''
         self._controller = controller
         self._crypto_letter_label = Tkinter.Label(self._parent_frame, text=self._crypto_letter, width=4, relief=Tkinter.RAISED)
         self._crypto_letter_label.grid(row=0, column=0)
-        self._plaintext_label = Tkinter.Label(self._parent_frame, text=self._plaintext_letter, width=4, relief=Tkinter.SUNKEN)
-        self._plaintext_label.bind("<Button-1>", self._on_click)
+        self._plaintext_label = Tkinter.Label(self._parent_frame, text=self._plaintext_letter, bg='gray80', width=4, relief=Tkinter.SUNKEN)
+        if self._is_alpha:
+            self._controller.register_letter(self)
+            self._plaintext_label.bind("<Button-1>", self._on_click)
         self._plaintext_label.grid(row=1, column=0)
 
     def _on_click(self, event):
@@ -60,5 +72,12 @@ class LetterView(object):
         return self._crypto_letter
 
     def set_plaintext_letter(self, letter):
-        self._plaintext_letter=letter
-        self._plaintext_label.config(text=letter)
+        if self._is_alpha:
+            self._plaintext_letter = letter
+            self._plaintext_label.config(text=letter)
+
+    def select(self):
+        self._plaintext_label.config(bg='yellow')
+
+    def deselect(self):
+        self._plaintext_label.config(bg='gray80')
